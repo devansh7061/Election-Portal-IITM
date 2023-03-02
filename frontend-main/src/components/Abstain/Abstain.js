83573924798ae85c419d6b58558eb342866c24d2
devansh7061 <devanshsaini67p@gmail.com>
2023-03-02 09:43:22 +0530

    preference abstain reject alert

--- frontend-main/src/components/Abstain/Abstain.js
+++ frontend-main/src/components/Abstain/Abstain.js
@@ -1,32 +1,69 @@
 import React, {useState} from 'react'
-import { Card, CardBody, Image } from "@chakra-ui/react";
+import {
+  Card,
+  CardBody,
+  Image,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  useDisclosure
+} from "@chakra-ui/react";
 import "./Abstain.css";
 import AbstainImage from "./Abstain.png"
 
-function handleClick(e, { active, setActive, variable, setVariable }) {
+function handleClick(e, { active, setActive, variable, setVariable, onOpen }) {
   e.preventDefault();
   setActive(!active);
+  console.log("Abstain variable", variable);
   if (variable == "Abstain") {
     setVariable(null);
-  } else {
+  } else if(variable == "Reject" || variable == null) {
     setVariable("Abstain");
+  } else {
+    if (variable.length > 1) {
+      onOpen();
+    }
+    setVariable("Abstain")
   }
 }
 
 function Abstain({ variable, setVariable }) {
   const [active, setActive] = useState(false);
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
-    <div className={variable == "Abstain" ? "active" : "inactive"}>
-      <Image
-        src={AbstainImage}
-        alt="Abstain"
-        borderRadius="lg"
-        boxSize="250px"
-        onClick={(e) =>
-          handleClick(e, { active, setActive, variable, setVariable })
-        }
-      />
-    </div>
+    <>
+      <div className={variable == "Abstain" ? "active" : "inactive"}>
+        <Image
+          src={AbstainImage}
+          alt="Abstain"
+          borderRadius="lg"
+          boxSize="250px"
+          onClick={(e) =>
+            handleClick(e, { active, setActive, variable, setVariable, onOpen })
+          }
+        />
+      </div>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Alert</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            You have already selected a preference, selecting this option will lead your vote to be counted as Abstained 
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   );
 }
 
--- frontend-main/src/components/Reject/Reject.js
+++ frontend-main/src/components/Reject/Reject.js
@@ -1,32 +1,63 @@
 import React, { useState } from "react";
-import { Card, CardBody, Image } from "@chakra-ui/react";
+import { Card, CardBody, Image, Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  useDisclosure } from "@chakra-ui/react";
 import "./Reject.css";
 import RejectImage from "./Reject.png";
 
-function handleClick(e, { active, setActive, setVariable, variable }) {
+function handleClick(e, { active, setActive, setVariable, variable, onOpen }) {
   e.preventDefault();
   setActive(!active);
   if (variable == "Reject") {
     setVariable(null);
+  } else if (variable == "Abstain" || variable == null) {
+    setVariable("Reject");
   } else {
+    if (variable.length > 1) {
+      onOpen();
+    }
     setVariable("Reject");
   }
 }
 
 function Reject({ variable, setVariable }) {
   const [active, setActive] = useState(false);
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
-    <div className={variable == "Reject" ? "active" : "inactive"}>
+    <>
+      <div className={variable == "Reject" ? "active" : "inactive"}>
       <Image
         src={RejectImage}
         alt="Reject"
         borderRadius="lg"
         boxSize="250px"
         onClick={(e) =>
-          handleClick(e, { active, setActive, variable, setVariable })
+          handleClick(e, { active, setActive, variable, setVariable, onOpen })
         }
       />
-    </div>
+      </div>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Alert</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            You have already selected a preference, selecting this option will lead your vote to be counted as Rejected 
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   );
 }
 
